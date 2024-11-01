550560bf8a56d5290f441154bec76174bcb21a74
akshat <a.oza@mail.utoronto.ca>
2024-11-01 16:17:53 -0400

    More data and modifications to the test rize and random split

--- backend/ml-model/model.py
+++ backend/ml-model/model.py
@@ -35,7 +35,7 @@ def model() -> float:
     _, _, target = file_reader()
 
     # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.6, random_state=100)
 
     # Initialize and train the model
     clf = tree.DecisionTreeClassifier()
