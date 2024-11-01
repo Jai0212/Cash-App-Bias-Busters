41a5242090c7f6768585d04ce2588102f7ee9877
armaganngul <armagan.gul@mail.utoronto.ca>
2024-11-01 16:26:48 -0400

    Fine tuned the model

--- backend/ml-model/model.py
+++ backend/ml-model/model.py
@@ -4,13 +4,26 @@ import pickle
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 
 def file_reader() -> (pd.DataFrame, pd.DataFrame, pd.Series): # type: ignore
     df = pd.read_csv('../../database/output.csv')
-    inputs = df.drop('is_biased', axis='columns')
-    target = df['is_biased']
-    return df, inputs, target
+
+    bins = range(18, 61, 10)
+
+    # Create labels for each bin
+    labels = [f"{i}-{i + 4}" for i in bins[:-1]]
+
+    # Use cut to create a new column with age groups
+    df['age_groups'] = pd.cut(df['age'], bins=bins, labels=labels, right=False)
+    df_dropped = df.drop('age', axis=1)
+    print(df_dropped)
+
+    inputs = df_dropped.drop('is_biased', axis='columns')
+    target = df_dropped['is_biased']
+
+    return df_dropped, inputs, target
 
 
 def labels_encoder() -> pd.DataFrame:
@@ -22,11 +35,11 @@ def labels_encoder() -> pd.DataFrame:
     _, inputs, _ = file_reader()
 
     inputs["gender_N"] = le_gender.fit_transform(inputs['gender'])
-    inputs["age_N"] = le_age.fit_transform(inputs['age'])
+    inputs["age_N"] = le_age.fit_transform(inputs['age_groups'])
     inputs["race_N"] = le_race.fit_transform(inputs['race'])
     inputs["state_N"] = le_state.fit_transform(inputs['state'])
 
-    inputs_n = inputs.drop(['gender', 'age', 'race', 'state', 'id', 'timestamp'], axis='columns')
+    inputs_n = inputs.drop(['gender', 'age_groups', 'race', 'state', 'id', 'timestamp'], axis='columns')
     return inputs_n
 
 
@@ -35,7 +48,7 @@ def model() -> float:
     _, _, target = file_reader()
 
     # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.2, random_state=None)
 
     # Initialize and train the model
     clf = tree.DecisionTreeClassifier()
