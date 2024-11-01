9c98eb5fa9ff9b1383d0304d9d9151d998403b87
armaganngul <armagan.gul@mail.utoronto.ca>
2024-11-01 16:38:12 -0400

    fine tuning boss

--- backend/ml-model/model.py
+++ backend/ml-model/model.py
@@ -48,11 +48,7 @@ def model() -> float:
     _, _, target = file_reader()
 
     # Split the data into training and test sets
-<<<<<<< HEAD
-    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.2, random_state=None)
-=======
-    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.6, random_state=100)
->>>>>>> 550560bf8a56d5290f441154bec76174bcb21a74
+    X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.2, random_state=48)
 
     # Initialize and train the model
     clf = tree.DecisionTreeClassifier()
