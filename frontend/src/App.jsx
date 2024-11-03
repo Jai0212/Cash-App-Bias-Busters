cbf0c3262e0f2299b4d9cd1ab204593c24ee0c6c
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-03 13:16:35 -0500

    added a comment for chartcomp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,43 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Importing Pages
-import UserSignup from './pages/User_signup.jsx';
-import UserLogin from './pages/Login.jsx';
-import Dashboard from './dashboard.jsx';
-import ChangePassword from './pages/ChangePassword.jsx';
-import ForgotPassword from './pages/ForgotPassword.jsx';
-import ResetPassword from './pages/ResetPassword.jsx';
+import UserSignup from "./pages/User_signup.jsx";
+import UserLogin from "./pages/Login.jsx";
+import Dashboard from "./dashboard.jsx";
+import ChangePassword from "./pages/ChangePassword.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
 
 // Importing Layouts
-import PublicLayout from './Layout/PublicLayout.jsx';
-import UserLayout from './Layout/UserLayout.jsx';
+import PublicLayout from "./Layout/PublicLayout.jsx";
+import UserLayout from "./Layout/UserLayout.jsx";
 
 const App = () => {
-    const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* Public Routes (Login and Signup) */}
-                <Route path="/" element={<PublicLayout />}>
-                    <Route path="signup" element={<UserSignup />} />
-                    <Route path="login" element={<UserLogin />} />
-                    <Route path="forgot_password" element={<ForgotPassword />} />
-                    <Route path="reset_password" element={<ResetPassword />} />
-                </Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes (Login and Signup) */}
+        <Route path="/" element={<PublicLayout />}>
+          <Route path="signup" element={<UserSignup />} />
+          <Route path="login" element={<UserLogin />} />
+          <Route path="forgot_password" element={<ForgotPassword />} />
+          <Route path="reset_password" element={<ResetPassword />} />
+        </Route>
 
-                {/* User Routes (Dashboard) */}
-                <Route path="/dashboard" element={<UserLayout />}>
-                    <Route path="" element={<Dashboard VITE_BACKEND_URL={VITE_BACKEND_URL} />} />
-                    <Route path="change-password" element={<ChangePassword />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+        {/* User Routes (Dashboard) */}
+        <Route path="/dashboard" element={<UserLayout />}>
+          <Route
+            path=""
+            element={<Dashboard VITE_BACKEND_URL={VITE_BACKEND_URL} />}
+          />
+          <Route path="change-password" element={<ChangePassword />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
