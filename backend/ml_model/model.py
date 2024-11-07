e50085622b98dfc955f7dc382df59ef1ab688fb1
armaganngul <armagan.gul@mail.utoronto.ca>
2024-11-07 16:32:05 -0500

    file_reader() turned clean

--- backend/ml_model/model.py
+++ backend/ml_model/model.py
@@ -11,6 +11,7 @@ from fairlearn.metrics import MetricFrame
 
 single_column_check = False
 current_dir = os.path.dirname(os.path.abspath(__file__))
+csv_file_path = os.path.join(current_dir, "../../database/output.csv")
 
 
 def file_reader() -> (pd.DataFrame, pd.DataFrame, pd.Series):  # type: ignore
@@ -24,37 +25,49 @@ def file_reader() -> (pd.DataFrame, pd.DataFrame, pd.Series):  # type: ignore
     action_status.
     """
 
-    csv_file_path = os.path.join(current_dir, "../../database/output.csv")
-    df = pd.read_csv(csv_file_path)
-
+    df = pd.read_csv(self.csv_file_path)
     df_cleaned = df.drop(["timestamp", "id"], axis=1, errors="ignore")
 
     # Check if the DataFrame has only one column
     if df_cleaned.shape[1] == 2:
         single_column_check = True
 
-    # Check if 'age' column exists
-    if "age" in df_cleaned.columns:
+    df_dropped = age_check(df_cleaned)
+    inputs = get_inputs(df_dropped)
+    target = get_target(df_dropped)
+
+    return df_dropped, inputs, target
+
+
+def get_target(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns the target of the dataframe on the parameter.
+    """
+    return df.get("action_status", pd.Series())  # Get 'action_status' or an empty Series if it does not exist
+
+
+def get_inputs(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns the inputs of the dataframe on the parameter.
+    """
+    # Ignore error if 'action_biased' does not exist
+    return df.drop("action_status", axis="columns", errors="ignore")
+
+
+def age_check(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function will check if the data frame entered has a age column, then convets it into age_groups where
+    the age groups are put into bins.
+    """
+    if "age" in df.columns:     # Check if 'age' column exists
+
         bins = range(18, 90, 9)
+        labels = [f"{i}-{i + 8}" for i in bins[:-1]]         # Create labels for each bin
 
-        # Create labels for each bin
-        labels = [f"{i}-{i + 8}" for i in bins[:-1]]
-
-        # Use cut to create a new column with age groups
-        df_cleaned["age_groups"] = pd.cut(
-            df_cleaned["age"], bins=bins, labels=labels, right=False
-        )
-
-    df_dropped = df_cleaned.drop("age", axis=1, errors="ignore")
-
-    inputs = df_dropped.drop(
-        "action_status", axis="columns", errors="ignore"
-    )  # Ignore error if 'is_biased' does not exist
-    target = df_dropped.get(
-        "action_status", pd.Series()
-    )  # Get 'is_biased' or an empty Series if it does not exist
-
-    return df_cleaned, inputs, target
+        df["age_groups"] = pd.cut(
+            df["age"], bins=bins, labels=labels, right=False        # Use cut to create a new column with age groups
+         )
+    return df.drop("age", axis=1, errors="ignore")
 
 
 def labels_encoder():
