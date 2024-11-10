181ea8a73433116b9f27e4eb90df2560f8e22395
akshat <a.oza@mail.utoronto.ca>
2024-11-10 17:45:10 -0500

    Imports corrected

--- backend/ml_model/use_cases/model.py
+++ backend/ml_model/use_cases/model.py
@@ -3,14 +3,22 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from fairlearn.metrics import MetricFrame
 import numpy as np
 import pandas as pd
+import sys
 import os
-from ml_model.data_access.file_reader import FileReader
-from ml_model.datapoint_entity import DataPoint
-from ml_model.data_access.model_saver import save_model
-from ml_model.preprocessing.data_preprocessing import DataProcessor
+
+
+# Add the root directory of the project to the sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
+
+# Now import the modules
+from ml_model.repository.file_reader import FileReader
+from ml_model.entities.datapoint_entity import DataPoint
+from ml_model.repository.model_saver import save_model
+from ml_model.repository.data_preprocessing import DataProcessor
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-csv_file_path = os.path.join(current_dir, "../../database/output.csv")
+csv_file_path = os.path.join(current_dir, "../../../database/output.csv")
 
 
 def model() -> list:
