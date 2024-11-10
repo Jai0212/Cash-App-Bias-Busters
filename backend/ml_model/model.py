0a5614997adc5a2ceda44b530fefd12ab9dfe951
akshat <a.oza@mail.utoronto.ca>
2024-11-09 21:32:12 -0500

    Split model up into 3 files for CLEAN

--- backend/ml_model/model.py
+++ backend/ml_model/model.py
@@ -1,114 +1,15 @@
-from typing import Any, Dict, List, Tuple
+from typing import Dict
 import math
-import os
-import pandas as pd
-import numpy as np
 import pickle
-from sklearn.preprocessing import LabelEncoder
+import numpy as np
 from sklearn import tree
 from sklearn.model_selection import train_test_split, GridSearchCV
 from fairlearn.metrics import MetricFrame
+from reader import file_reader
+from labels_encoder import labels_encoder
+
 
 single_column_check = False
-current_dir = os.path.dirname(os.path.abspath(__file__))
-csv_file_path = os.path.join(current_dir, "../../database/output.csv")
-categorical_columns = ["gender", "age_groups", "race", "state",]
-
-
-def file_reader() -> (pd.DataFrame, pd.DataFrame, pd.Series):  # type: ignore
-    """
-    This function will read the csv file.
-    Then it will check whether there is only one column in the cleaned file
-    after dropping timestamp and id.
-    If age is one of these columns it will have to create bins based on age
-    groups incremented by 9.
-    Then our target is action_status, and it returns the cleaned file, inputs and
-    action_status.
-    """
-
-    df = pd.read_csv(self.csv_file_path)
-    df_cleaned = df.drop(["timestamp", "id"], axis=1, errors="ignore")
-
-    if df_cleaned.shape[1] == 2:        # Check if the DataFrame has only one column
-        self.single_column_check = True
-
-    df_dropped = age_check(df_cleaned)
-    inputs = get_inputs(df_dropped)
-    target = get_target(df_dropped)
-
-    return df_dropped, inputs, target
-
-
-def get_target(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Returns the target of the dataframe on the parameter.
-    """
-    return df.get("action_status", pd.Series())  # Get 'action_status' or an empty Series if it does not exist
-
-
-def get_inputs(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Returns the inputs of the dataframe on the parameter.
-    """
-    # Ignore error if 'action_biased' does not exist
-    return df.drop("action_status", axis="columns", errors="ignore")
-
-
-def age_check(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    This function will check if the data frame entered has a age column, then convets it into age_groups where
-    the age groups are put into bins.
-    """
-    if "age" in df.columns:     # Check if 'age' column exists
-
-        bins = range(18, 90, 9)
-        labels = [f"{i}-{i + 8}" for i in bins[:-1]]         # Create labels for each bin
-
-        df["age_groups"] = pd.cut(
-            df["age"], bins=bins, labels=labels, right=False        # Use cut to create a new column with age groups
-         )
-    return df.drop("age", axis=1, errors="ignore")
-
-
-def labels_encoder():
-    """
-    Orchestrates label encoding and returns encoded data along with mappings.
-    """
-    _, inputs, _ = file_reader()
-    le_dict = {}
-    inputs = encode_categorical_columns(inputs, le_dict)
-    mappings = get_mappings(le_dict)
-    inputs_n = drop_categorical_columns(inputs)
-    return inputs_n, mappings
-
-
-def encode_categorical_columns(inputs: pd.DataFrame, le_dict: dict) -> pd.DataFrame:
-    """
-    Encodes each categorical column using LabelEncoder.
-    """
-    for col in self.categorical_columns:
-        if col in inputs.columns:
-            le = LabelEncoder()
-            inputs[f"{col}_N"] = le.fit_transform(inputs[col])
-            le_dict[col] = le
-    return inputs
-
-
-def get_mappings(le_dict: dict) -> dict:
-    """
-    Retrieves mappings from encoded values to original labels.
-    """
-    return {
-        col: dict(zip(le.transform(le.classes_), le.classes_))
-        for col, le in le_dict.items()
-    }
-
-
-def drop_categorical_columns(inputs: pd.DataFrame) -> pd.DataFrame:
-    """
-    Drops the original categorical columns and returns necessary columns.
-    """
-    return inputs.drop(columns=self.categorical_columns, errors="ignore")
 
 
 def model() -> dict:
@@ -119,9 +20,8 @@ def model() -> dict:
     _, _, target = file_reader()  # Get the target variable
 
     # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        inputs, target, test_size=0.2, random_state=48
-    )
+    x_train, x_test, y_train, y_test = (
+        train_test_split(inputs, target, test_size=0.2, random_state=48))
 
     # Define the model
     clf = tree.DecisionTreeClassifier()
@@ -137,23 +37,23 @@ def model() -> dict:
     grid_search = GridSearchCV(clf, param_grid, cv=5, scoring="accuracy")
 
     # Fit the model
-    grid_search.fit(X_train, y_train)
+    grid_search.fit(x_train, y_train)
 
     # Get the best model from grid search
     best_clf = grid_search.best_estimator_
 
     # Get predictions
-    y_pred = best_clf.predict(X_test)
+    y_pred = best_clf.predict(x_test)
     feature1 = inputs.columns[0]
 
     # Specify multiple sensitive features
     if (
             "single_column_check" in globals() and single_column_check
     ):  # Ensure single_column_check is defined
-        sensitive_features = X_test[[feature1]]  # Add your sensitive features here
+        sensitive_features = x_test[[feature1]]  # Add your sensitive features here
     else:
         feature2 = inputs.columns[1]
-        sensitive_features = X_test[
+        sensitive_features = x_test[
             [feature1, feature2]
         ]  # Add your sensitive features here
 
@@ -177,7 +77,7 @@ def model() -> dict:
     print(metric_frame.by_group)
 
     # Overall model score
-    score = best_clf.score(X_test, y_test)
+    score = best_clf.score(x_test, y_test)
 
     # Save the model and its score
     with open("model_with_score.pkl", "wb") as f:
