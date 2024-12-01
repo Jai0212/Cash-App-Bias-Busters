eafba3c419322e3a8f672770fa4d3f0625a15752
tvirat <155598251+tvirat@users.noreply.github.com>
2024-12-01 00:40:29 -0500

    added test qrcode

---
+++ frontend/tests/DashboardPage/QRCodeShare.test.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import QRCodeShare from "../../src/pages/DashboardPage/QRCodeShare/QRCodeShare";
+import { BrowserRouter } from "react-router-dom";
+
+jest.mock("../../src/envConfig", () => ({
+  envConfig: () => "test",
+}));
+
+describe("QRCodeShare Component", () => {
+  const selectedDemographic = "age";
+  const selectedValues = [25, 30];
+  const selectedSecondValues = [35, 40];
+  const secondSelectedDemographic = "gender";
+  const timeframe = "2022";
+  const currUser = "testUser";
+
+  it("renders share button", () => {
+    render(
+      <BrowserRouter>
+        <QRCodeShare
+          selectedDemographic={selectedDemographic}
+          selectedValues={selectedValues}
+          selectedSecondValues={selectedSecondValues}
+          secondSelectedDemographic={secondSelectedDemographic}
+          timeframe={timeframe}
+          currUser={currUser}
+        />
+      </BrowserRouter>
+    );
+
+    const shareButton = screen.getByRole("button", { name: /share/i });
+    expect(shareButton).toBeInTheDocument();
+  });
+});
